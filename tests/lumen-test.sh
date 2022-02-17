6ff358f9918a9071304b8fa04e6d4a5dbd13d1cc
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-02-17 22:13:32 +0100

    try ignoring dev deps

--- tests/lumen-test.sh
+++ tests/lumen-test.sh
@@ -11,7 +11,7 @@ cd ../lumen/
 
 echo "Adding package from source"
 sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i composer.json
-COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*" -W
+COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*" -W --update-no-devs
 
 echo "Analyzing Lumen"
 ./vendor/bin/psalm -c ../psalm-plugin-laravel/tests/lumen-test-psalm.xml
